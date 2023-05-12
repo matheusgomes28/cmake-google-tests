4464812a9918da213c626be560c9a1062a5c35a0
matheusgomes28 <matheusgarcia28@gmail.com>
2023-05-12 21:35:50 +0200

    Adding basic CMake CI (#4)

--- vcpkg/src/multiply/multiply.cpp
+++ vcpkg/src/multiply/multiply.cpp
@@ -2,16 +2,15 @@
 
 int multiply(int a, int b)
 {
-    return a*b;
+    return a * b;
 }
 
 float multiply(float a, float b)
 {
-    return a*b;
+    return a * b;
 }
 
 double multiply(double a, double b)
 {
-    return a*b;
+    return a * b;
 }
-
