0657ecf6d0bbc4145ba4f71763ce52b1e00d3dc1
matheusgomes28 <matheusgarcia28@gmail.com>
2023-05-17 21:31:27 +0100

    upgrade docker, add vcpkg install build.

--- scripts/helpers/vcpkg-install.sh
+++ scripts/helpers/vcpkg-install.sh
@@ -2,20 +2,8 @@
 
 set -euo pipefail
 
-: ' This file will install vcpkg and export the VCPKG_ROOT
-    variable to the environment.
+: ' install vcpkg dependencies. this script was
+    created to pass as argument to the VCPKG builds.
 '
 
-
-apt-get update -y
-apt-get install zip -y
-
-git clone https://github.com/Microsoft/vcpkg.git
-
-./vcpkg/bootstrap-vcpkg.sh
-
-VCPKG_ROOT="$(realpath "vcpkg/")"
-export VCPKG_ROOT
-
-PATH="${PATH}:${VCPKG_ROOT}"
-export PATH
+vcpkg install 
